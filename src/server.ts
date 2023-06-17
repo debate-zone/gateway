2429293233daf54b1009fe03c1a496bc65eb9827
Grigori Fiodorov <grigori@q-ueue.ai>
2023-06-17 19:57:57 +0300

    moved targets to env

--- src/server.ts
+++ src/server.ts
@@ -11,32 +11,38 @@ const server = gateway({
     routes: [
         {
             prefix: '/user',
-            target: 'http://localhost:8090'
-
+            target: process.env.USER_MICRO_SERVICE_URL,
         },
         {
             prefix: '/debate-zone',
-            target: 'http://localhost:8092',
+            target: process.env.DEBATE_ZONE_MICRO_SERVICE_URL,
+            middlewares: [
+                checkToken
+            ],
+        },
+        {
+            prefix: '/comment',
+            target: process.env.COMMENT_MICRO_SERVICE_URL,
             middlewares: [
                 checkToken
             ],
         },
         {
             prefix: '/notification',
-            target: 'http://localhost:8093',
+            target: process.env.NOTIFICATION_MICRO_SERVICE_URL,
             middlewares: [
                 checkToken
             ],
         },
         {
             prefix: '/streaming',
-            target: 'http://localhost:8091'
+            target: process.env.STREAMING_MICRO_SERVICE_URL,
         },
     ]
 })
 
-server.start(8080).then(() => {
-    console.log('Gateway started!')
+server.start(process.env.PORT).then(() => {
+    console.log('Server started on port ' + process.env.PORT)
 }).catch((err: any) => {
     console.error(err)
 })
