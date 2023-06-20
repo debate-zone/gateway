3b53494060077c3ef627b13021724bc69c177ffc
Grigori Fiodorov <grigori@q-ueue.ai>
2023-06-20 21:27:22 +0300

    feat: Auth header and prettier
    added fullname, role, email to header
    prettier

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,47 @@
-import 'dotenv/config';
-import {checkToken} from "./middlewares/authMiddlewares";
-const gateway = require('fast-gateway')
+import "dotenv/config";
+import { checkToken } from "./middlewares/authMiddlewares";
+const gateway = require("fast-gateway");
 
 export type JWT = {
-    userId?: string;
-    role?: string;
+  userId: string;
+  role: string;
+  email: string;
+  fullName: string;
 };
 
 const server = gateway({
-    routes: [
-        {
-            prefix: '/user',
-            target: process.env.USER_MICRO_SERVICE_URL,
-        },
-        {
-            prefix: '/debate-zone',
-            target: process.env.DEBATE_ZONE_MICRO_SERVICE_URL,
-            middlewares: [
-                checkToken
-            ],
-        },
-        {
-            prefix: '/comment',
-            target: process.env.COMMENT_MICRO_SERVICE_URL,
-            middlewares: [
-                checkToken
-            ],
-        },
-        {
-            prefix: '/notification',
-            target: process.env.NOTIFICATION_MICRO_SERVICE_URL,
-            middlewares: [
-                checkToken
-            ],
-        },
-        {
-            prefix: '/streaming',
-            target: process.env.STREAMING_MICRO_SERVICE_URL,
-        },
-    ]
-})
+  routes: [
+    {
+      prefix: "/user",
+      target: process.env.USER_MICRO_SERVICE_URL,
+    },
+    {
+      prefix: "/debate-zone",
+      target: process.env.DEBATE_ZONE_MICRO_SERVICE_URL,
+      middlewares: [checkToken],
+    },
+    {
+      prefix: "/comment",
+      target: process.env.COMMENT_MICRO_SERVICE_URL,
+      middlewares: [checkToken],
+    },
+    {
+      prefix: "/notification",
+      target: process.env.NOTIFICATION_MICRO_SERVICE_URL,
+      middlewares: [checkToken],
+    },
+    {
+      prefix: "/streaming",
+      target: process.env.STREAMING_MICRO_SERVICE_URL,
+    },
+  ],
+});
 
-server.start(process.env.PORT).then(() => {
-    console.log('Server started on port ' + process.env.PORT)
-}).catch((err: any) => {
-    console.error(err)
-})
+server
+  .start(process.env.PORT)
+  .then(() => {
+    console.log("Server started on port " + process.env.PORT);
+  })
+  .catch((err: any) => {
+    console.error(err);
+  });
